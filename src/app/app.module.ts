e1ee26164b69001a8c4337b8a170de8d11aec128
Peter Melisko <pmelisko@gratex.com>
2021-02-09 13:23:38 +0100

    [SAMPLE] prepare sample

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot()
+  ],
+  declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
